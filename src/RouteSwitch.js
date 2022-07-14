b6662c54bd488e935ddf87ffb9e59728682e8194
rohannaido <87189343+rohannaido@users.noreply.github.com>
2022-07-14 20:33:42 +0530

    fix: for GitHub deployment

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Shop from "./Shop";
 import CartPage from "./CartPage";
@@ -9,13 +9,13 @@ const RouteSwitch = () => {
   const [ cart, setCart ] = useState([]);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App cart={cart} setCart={setCart} />} />
         <Route path="/shop" element={<Shop cart={cart} setCart={setCart} />} />
         <Route path="/cart" element={<CartPage cart={cart} setCart={setCart} />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
